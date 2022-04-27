4b77ee0125f5fe8ea57a1433786cf4815793cd0e
Asawer Khan <asawerkhan01@gmail.com>
2022-04-27 09:54:15 +0100

    AK: init commit

---
+++ src/main/java/com/qa/baespring/ReadingListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.baespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingListProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingListProjectApplication.class, args);
+	}
+
+}
