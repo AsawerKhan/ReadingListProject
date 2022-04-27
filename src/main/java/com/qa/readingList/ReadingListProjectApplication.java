b9d60a3fdfdc03027fabd79a20942fff31c15107
Asawer Khan <asawerkhan01@gmail.com>
2022-04-27 12:07:27 +0100

    AK: Entity class created

--- src/main/java/com/qa/readingList/ReadingListProjectApplication.java
+++ src/main/java/com/qa/readingList/ReadingListProjectApplication.java
@@ -1,4 +1,4 @@
-package com.qa.baespring;
+package com.qa.readingList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/qa/readingList/domain/Books.java
@@ -0,0 +1,11 @@
+package com.qa.readingList.domain;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Books {
+
+	
+
+}
