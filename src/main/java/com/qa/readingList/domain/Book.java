b7451551c46bc6cfb93d7685d0b84b6c46ba43ac
Asawer Khan <asawerkhan01@gmail.com>
2022-04-27 12:43:05 +0100

    AK: Generated getters and setters

--- src/main/java/com/qa/readingList/domain/Book.java
+++ src/main/java/com/qa/readingList/domain/Book.java
@@ -64,6 +64,51 @@ public class Book {
 		this.readingStatus = readingStatus;
 		this.isbn = isbn;
 	}
+
+
+	// Getters and setters
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getReadingStatus() {
+		return readingStatus;
+	}
+	public void setReadingStatus(String readingStatus) {
+		this.readingStatus = readingStatus;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 	
 	
 	
