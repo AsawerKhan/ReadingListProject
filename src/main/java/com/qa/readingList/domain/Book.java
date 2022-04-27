70c3db696df3a9a4a08b7bd32bfa9bd52c712c2d
Asawer Khan <asawerkhan01@gmail.com>
2022-04-27 12:28:30 +0100

    AK: Generated constructors

--- src/main/java/com/qa/readingList/domain/Book.java
+++ src/main/java/com/qa/readingList/domain/Book.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 @Entity
 public class Book {
 
-	// Private Properties
+	// Private properties
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +30,40 @@ public class Book {
 	
 	@Column(nullable = true, unique = true)
 	private String isbn;
+	
+	
+	
+	
+	// Default constructor
+	
+	public Book() {}
+	
+	
+	
+	// Constructor for creating and inserting
+	
+	public Book(String bookName, String author, String genre, String readingStatus, String isbn) {
+		super();
+		this.bookName = bookName;
+		this.author = author;
+		this.genre = genre;
+		this.readingStatus = readingStatus;
+		this.isbn = isbn;
+	}
+
+
+
+	// Constructor for reading, selecting and testing
+	
+	public Book(long id, String bookName, String author, String genre, String readingStatus, String isbn) {
+		super();
+		this.id = id;
+		this.bookName = bookName;
+		this.author = author;
+		this.genre = genre;
+		this.readingStatus = readingStatus;
+		this.isbn = isbn;
+	}
 	
 	
 	
