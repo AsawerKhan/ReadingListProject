ca61a9fa8cf0c9d968d83a963437fbfaf3a46258
Asawer Khan <asawerkhan01@gmail.com>
2022-04-28 21:17:36 +0100

    AK: Controller Unit Test updated with content

--- src/test/java/com/qa/readingList/controller/BookControllerUnitTest.java
+++ src/test/java/com/qa/readingList/controller/BookControllerUnitTest.java
@@ -11,22 +11,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.readingList.domain.Book;
+import com.qa.readingList.service.BookService;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Sql(scripts = {"classpath:testschema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
-@ActiveProfiles("test")
+@WebMvcTest
 public class BookControllerUnitTest {
 
 	@Autowired
@@ -35,21 +31,8 @@ public class BookControllerUnitTest {
 	@Autowired
 	private ObjectMapper mapper;
 	
-	// Create
-	@Test
-	public void createTest() throws Exception {
-		Book entry = new Book("The Book Thief", "Markus Zusak", "Historical Fiction", "To Be Read", "ISBN: 9781784162122");
-		String entryAsJSON = mapper.writeValueAsString(entry);
-		
-		Book result = new Book(2L, "The Book Thief", "Markus Zusak", "Historical Fiction", "To Be Read", "ISBN: 9781784162122");
-		String resultAsJSON = mapper.writeValueAsString(result);
-		
-		mvc.perform(post("/book/create")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(entryAsJSON))
-				.andExpect(status().isCreated())
-				.andExpect(content().json(resultAsJSON));
-	}
+	@MockBean
+	private BookService service;
 	
 	// getAll
 	@Test
@@ -58,6 +41,8 @@ public class BookControllerUnitTest {
 		List<Book> output = new ArrayList<>();
 		output.add(book);
 		String outputAsJSON = mapper.writeValueAsString(output);
+		
+		Mockito.when(this.service.getAll()).thenReturn(output);
 		
 		mvc.perform(get("/book/getAll")
 				.contentType(MediaType.APPLICATION_JSON))
@@ -71,9 +56,26 @@ public class BookControllerUnitTest {
 		Book entry = new Book(1L, "Coding for Dummies", "Nikhil Abraham", "Educational", "To Be Read", "ISBN: 9781119293323");
 		String entryAsJSON = this.mapper.writeValueAsString(entry);
 		
+		Mockito.when(this.service.getById(1L)).thenReturn(entry);
+		
 		mvc.perform(get("/book/getById/1")
 			.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
+			.andExpect(content().json(entryAsJSON));
+	}
+	
+	// Create
+	@Test
+	void createTest() throws Exception {
+		Book entry = new Book("The Book Thief", "Markus Zusak", "Historical Fiction", "To Be Read", "ISBN: 9781784162122");
+		String entryAsJSON = this.mapper.writeValueAsString(entry);
+		
+		Mockito.when(this.service.create(entry)).thenReturn(entry);
+		
+		mvc.perform(post("/book/create")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(entryAsJSON))
+			.andExpect(status().isCreated())
 			.andExpect(content().json(entryAsJSON));
 	}
 	
@@ -81,22 +83,34 @@ public class BookControllerUnitTest {
 	@Test
 	public void updateTest() throws Exception {
 		Book entry = new Book("The Book Thief", "Markus Zusak", "Historical Fiction", "To Be Read", "ISBN: 9781784162122");
-		Book result = new Book(1L, "The Book Thief", "Markus Zusak", "Historical Fiction", "To Be Read", "ISBN: 9781784162122");
 		String entryAsJSON = this.mapper.writeValueAsString(entry);
-		String resultAsJSON = this.mapper.writeValueAsString(result);
+		
+		Mockito.when(this.service.update(1L, entry)).thenReturn(entry);
 		
 		mvc.perform(put("/book/update/1")
 			.contentType(MediaType.APPLICATION_JSON)
 			.content(entryAsJSON))
 			.andExpect(status().isAccepted())
-			.andExpect(content().json(resultAsJSON));
+			.andExpect(content().json(entryAsJSON));
 	}
 	
 	// Delete
 	@Test
 	public void deleteTest() throws Exception {
+		Mockito.when(this.service.delete(1L)).thenReturn(true);
+		
 		mvc.perform(delete("/book/delete/1")
 			.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isNoContent());
+	}
+	
+	
+	@Test
+	public void deleteFailTest() throws Exception {
+		Mockito.when(this.service.delete(2L)).thenReturn(false);
+	
+		mvc.perform(delete("/book/delete/5")
+			.contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isInternalServerError());
 	}
 }
