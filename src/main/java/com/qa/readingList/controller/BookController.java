cb5efeb0e1019ca1acad40208009732edc165b37
Asawer Khan <asawerkhan01@gmail.com>
2022-04-28 14:44:44 +0100

    AK: Removed an import

--- src/main/java/com/qa/readingList/controller/BookController.java
+++ src/main/java/com/qa/readingList/controller/BookController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qa.baespring.domain.User;
 import com.qa.readingList.domain.Book;
 import com.qa.readingList.service.BookService;
 
